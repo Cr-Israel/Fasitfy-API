9df8970afa20dc5f08f743dcdf5474fbe8221a8e
Luís Felipe <luis.felipeszsv@gmail.com>
2023-06-12 08:59:57 -0300

    refactor: change initial heroes to uuids

--- db/Heroes.js
+++ db/Heroes.js
@@ -1,7 +1,7 @@
 let heroes = [
-    {id: '1', name: 'Batman'},
-    {id: '2', name: 'Asa Noturna'},
-    {id: '3', name: 'Programador'},
+    {id: 'af7c1fe6-d669-414e-b066-e9733f0de7a8', name: 'Batman'},
+    {id: 'c8d4c641-4b14-4736-8e60-bdc01d5ee8fe', name: 'Asa Noturna'},
+    {id: '2e46c0aa-011f-4fc2-81e8-5365de43fff7', name: 'Programador'},
 ];
 
 module.exports = heroes;
